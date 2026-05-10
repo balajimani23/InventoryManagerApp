8827a89258ed2a83db343d3731310aa781e50d16
balajimani23 <37340753+balajimani23@users.noreply.github.com>
2026-05-10 16:02:10 +0530

    Initial Commit

---
+++ src/test/java/com/supply/bridge/InventoryManagerApp/InventoryManagerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supply.bridge.InventoryManagerApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
